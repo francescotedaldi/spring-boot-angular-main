d58a005e817d451420583ea710aa031540650763
francescotedaldi <francescoteda@gmail.com>
2022-04-29 12:20:36 +0200

    prova login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  public submitted: boolean;
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.submitted = false;
+    this.buildForm();
   }
 
+  public buildForm(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required)
+    });
+  }
+
+  private loginHandler(): void{
+    this.submitted = true;
+
+    console.log('login: loginHandler: submitted = true;', this.form);
+  } 
+  
+  // convenience getter for easy access to form fields
+  public get f(): { [p: string]: AbstractControl } {
+    return this.form.controls;
+  }
 }
